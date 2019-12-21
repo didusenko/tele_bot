64ee7d9b7ac6d9cffafb9681279240c720ba5771
didusenko <underyura@gmail.com>
2019-12-21 19:23:30 +0200

    Update didusevich_bot.py

--- didusevich_bot.py
+++ didusevich_bot.py
@@ -1,13 +1,39 @@
 import telebot
-bot = telebot.TeleBot("!!!!!!!!!!!!!!!!!!!!!!token")
+
+bot = telebot.TeleBot("*************************************")
+
+link1 = 'https://my.tec.cn.ua/wb4tecls/login/auth'
+link2 = 'https://stat.water.cn.ua/wb2abvdk/login/auth'
+link3 = 'https://chernigivoblenergo.com.ua/abon_cabinet/'
 
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
-    msg = bot.send_message(message.chat.id, 'Привет! Я твой личный бот\n' + '\n' + 'Чем могу помочь?')
-    msg = bot.send_message(message.chat.id, '/help - список доступных комманд')
+def welcome(message):
     markup = telebot.types.InlineKeyboardMarkup()
-    button_1 = telebot.types.InlineKeyboardButton(text='help', switch_inline_query_current_chat='/help')
+    button_1 = telebot.types.InlineKeyboardButton(text='Начать!!!', callback_data='main_page')
     markup.add(button_1)
-    bot.send_message(message.chat.id, "Нажми на кнопку и перейди на наш сайт.", reply_markup = markup)
+    bot.send_message(message.chat.id, text='Привет Хозяин! Я твой личный помощник', reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda call: True)
+def process_callback(call):
+    if call.data == 'main_page':
+        markup = telebot.types.InlineKeyboardMarkup()
+        button_0 = telebot.types.InlineKeyboardButton(text='Главная Страница', callback_data='main_page')
+        button_1 = telebot.types.InlineKeyboardButton(
+            text='Передать показания счетчиков (вода, електричество)', callback_data='btm1')
+        markup.add(button_0)
+        markup.add(button_1)
+        bot.send_message(
+            call.message.chat.id, text='Преветствую в главном меню, Выберите нужное действие!', reply_markup=markup)
+
+    elif call.data == 'btm1':
+        markup = telebot.types.InlineKeyboardMarkup()
+        button_0 = telebot.types.InlineKeyboardButton(text='Главная Страница', callback_data='main_page')
+        button_1 = telebot.types.InlineKeyboardButton(text='Обленерго', url=link1)
+        button_2 = telebot.types.InlineKeyboardButton(text='Водоканал', url=link2)
+        button_3 = telebot.types.InlineKeyboardButton(text='Тец', url=link3)
+        markup.add(button_0)
+        markup.add(button_1, button_2, button_3)
+        bot.send_message(call.message.chat.id, text='Выберите нужный ресурс:', reply_markup=markup)
+
 
 bot.polling(none_stop=True, interval=0)
